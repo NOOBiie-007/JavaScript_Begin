7958355427d9edb78c8c1042688958c6224ff48c
NOOBiie-007 <77447769+NOOBiie-007@users.noreply.github.com>
2022-04-28 23:08:08 +0600

    For and While and Switch

--- begin.js
+++ begin.js
@@ -9,3 +9,12 @@ console.log(array);
 array.pop();
 console.log(array);
 console.log(array.length);
+array.shift();
+console.log(array);
+array.unshift(48);
+console.log(array);
+
+var sli = array.slice(2);
+console.log(sli);
+var sli = array.slice(2,5);
+console.log(sli);
