b2dc11016192f50f05a209ccb7a3f29c9bccf2fd
NOOBiie-007 <77447769+NOOBiie-007@users.noreply.github.com>
2022-04-28 16:26:37 +0600

    first JS

---
+++ begin.js
@@ -0,0 +1,11 @@
+var array=[12,45,32,67];
+console.log(array);
+console.log(array[1]);
+var position = array.indexOf(32);
+console.log(position);
+
+array.push(10);
+console.log(array);
+array.pop();
+console.log(array);
+console.log(array.length);
